a3788f07b81a47948fea0453a4baa510071f9ef6
ivo2666 <ivailocvetkov365@gmail.com>
2022-12-30 14:03:54 +0200

    feat: Add nav-list component to storybook

---
+++ src/components/nav-list/nav-list.tsx
@@ -0,0 +1,18 @@
+import classes from "../../styles/nav-list.module.scss";
+import { FC, memo } from "react";
+
+/**
+ * UI component for application nav
+ */
+export const NavList: FC = ({
+  ...props
+}) => {
+  return (
+    <ul className={classes["nav-list"]} {...props}>
+        <li className={classes["nav-list-item"]}>About</li>
+        <li className={classes["nav-list-item"]}>Call Us</li>
+    </ul>
+  );
+};
+
+export default memo(NavList);
---
+++ src/stories/nav-list.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import "../fonts/Poppins/Poppins-SemiBold.ttf";
+import "../index.css";
+import { NavList } from "../components/nav-list";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Nav List",
+  component: NavList,
+} as ComponentMeta<typeof NavList>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof NavList> = (args) => <NavList {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+};
