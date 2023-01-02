89aa083dc9602f9e2603fb6a86ae505ef0bc042e
ivo2666 <ivailocvetkov365@gmail.com>
2023-01-02 19:07:32 +0200

    feat: Add App Layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 
+import BasketStat from './components/basket-stat/basket-stat';
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import ProductList from './components/product-list/product-list';
+import classes from './styles/app.module.scss';
+import data from './data/data.json';
+
 function App() {
   return (
-    <div className="App">
-    
+    <div className={classes.app}>
+      <Header />
+      <Hero className={classes.hero} />
+      <div className={classes["product-list-wrapper"]}><ProductList items={data.items} /></div>
+      <BasketStat className={classes["basket-stat"]} totalPrice={20} />
     </div>
   );
 }
